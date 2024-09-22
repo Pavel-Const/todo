16b11b0fecbaa26164c28193b1fe246e6934cf57
Pavel-Const <64945259+Pavel-Const@users.noreply.github.com>
2024-09-22 12:12:04 +0600

    fix bug with dnd

--- src/components/TaskList/TaskList.tsx
+++ src/components/TaskList/TaskList.tsx
@@ -1,36 +1,41 @@
-import {useSelector} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
 import {RootState} from "../../store/store.ts";
 import styles from "./taskList.module.scss";
 import {TaskItem} from "./TaskItem/TaskItem.tsx";
 import {DndProvider} from "react-dnd";
 import {HTML5Backend} from "react-dnd-html5-backend";
 import {useCallback, useEffect, useState} from "react";
+import {setTasks} from "../../store/slices/taskSlice.ts";
 
 export default function TaskList() {
     const {tasks, filter} = useSelector((state: RootState) => state.tasks);
     const [taskOrder, setTaskOrder] = useState(tasks);
-    
+    const dispatch = useDispatch();
     useEffect(() => {
-        setTaskOrder(tasks)
-    }, [tasks]);
+        if (filter === "completed") {
+            setTaskOrder(tasks.filter(item => item.completed));
+        } else if (filter === "pending") {
+            setTaskOrder(tasks.filter(item => !item.completed));
+        } else {
+            setTaskOrder([...tasks]);
+        }
+    }, [filter, tasks]);
     
-    
-    const filteredTasks = taskOrder.filter((task) => {
-        if (filter === "completed") return task.completed;
-        if (filter === "pending") return !task.completed;
-        return true;
-    });
     const moveTask = useCallback((dragIndex: number, hoverIndex: number) => {
-        const updatedTasks = [...taskOrder];
-        const [movedTask] = updatedTasks.splice(dragIndex, 1);
-        updatedTasks.splice(hoverIndex, 0, movedTask);
-        setTaskOrder(updatedTasks);
-    }, [taskOrder]);
+        if (filter === "all") {
+            const dragTask = taskOrder[dragIndex];
+            const updatedTasks = [...taskOrder];
+            updatedTasks.splice(dragIndex, 1);
+            updatedTasks.splice(hoverIndex, 0, dragTask);
+            setTaskOrder(updatedTasks);
+            dispatch(setTasks(updatedTasks))
+        }
+    }, [taskOrder, filter]);
     
     return (
         <DndProvider backend={HTML5Backend}>
             <ul className={styles.list}>
-                {filteredTasks.map((task, index) => (
+                {taskOrder.map((task, index) => (
                     <TaskItem task={task} key={task.id} moveTask={moveTask} index={index}/>
                 ))}
             </ul>
